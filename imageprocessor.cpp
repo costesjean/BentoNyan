77d6c47c707b7457b0e8e5ae9d01cc2c396b2487
timalherbe <malherbe.timote@sfr.fr>
2018-05-24 12:00:15 +0200

    J'ai RE oublié d'enregistrer avant de push

--- imageprocessor.cpp
+++ imageprocessor.cpp
@@ -4,3 +4,17 @@ ImageProcessor::ImageProcessor()
 {
 
 }
+
+vector<Scalar> computeAverage(Mat img){
+    vector<Mat> channels;
+    split(img, chanels);
+    Scalar r = mean(channels[0]);
+    Scalar g = mean(channels[1]);
+    Scalar b = mean(channels[2]);
+
+    vector<Scalar> averages;
+    averages.add(r[0]);
+    averages.add(g[0]);
+    averages.add(b[0]);
+    return averages;
+}
